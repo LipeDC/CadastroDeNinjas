34703c6b6da803e28fd03260f6761ccdb22ca2c6
LipeDC <felipinhodias75@gmail.com>
2025-07-01 20:40:58 -0300

    Adicionando Lombok

--- src/main/java/com/lipe/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/com/lipe/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -2,10 +2,16 @@ package com.lipe.CadastroDeNinjas.Ninjas.Controller.Service;
 
 import com.lipe.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity transforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -21,37 +27,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String name, String email, int idade) {
-        this.name = name;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
